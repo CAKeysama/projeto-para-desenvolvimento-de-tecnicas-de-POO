df2c582338ed295dd75cdbf28650c59d082134a0
CAkeysama <gustavodomingues2004gghz@gmail.com>
2023-03-08 10:24:16 -0300

    criado classe pessoa.

---
+++ coiso do ferneko/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coiso_do_ferneko
+{
+    internal class Pessoa
+    {
+        public int Id;
+        public string Name;
+        public string Telefone;
+
+        public Pessoa(int id, string name, string telefone)
+        {
+            Id = id;
+            Name = name;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id; 
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public void setName(string name)
+        {
+            Name = name;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    
+    }
+}
+
+    
