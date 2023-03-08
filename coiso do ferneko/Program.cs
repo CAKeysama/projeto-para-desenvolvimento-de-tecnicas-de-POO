38f6de623498a12b0334c3c2af36c6211af450ba
CAkeysama <gustavodomingues2004gghz@gmail.com>
2023-03-08 10:05:29 -0300

    criados os metodos set para cada elemento do obj Produto. adicionado um texte no program.cs.

--- coiso do ferneko/Program.cs
+++ coiso do ferneko/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("hallo zauardooo");
+﻿using coiso_do_ferneko;
+
+Console.WriteLine("hallo zauardooo");
+
+Produto p1 = new Produto(1, "mouse sem fio", 59, 90M);
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
